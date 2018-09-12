5bf8a5339061b0d69f022244e3dd4ff42c0848b5
huitoukest <huitoukest@foxmail.com>
2018-09-12 17:29:21 +0800

    add

--- src/main/java/com/tingfeng/util/java/base/common/utils/JDKProxyUtils.java
+++ src/main/java/com/tingfeng/util/java/base/common/utils/JDKProxyUtils.java
@@ -13,7 +13,7 @@ public class JDKProxyUtils {
 	  * @param interfaceClass 接口类
 	  * @return (实现接口的)代理方法
 	  */
-	public static <T,E extends T> T getProxy(Class<T> interfaceClass,InvocationHandler invocationHandler){
+	public static <T> T getProxy(Class<T> interfaceClass,InvocationHandler invocationHandler){
 		 Object proxy = Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(),
 	                new Class[] { interfaceClass },invocationHandler); 
 		 return (T) proxy;
