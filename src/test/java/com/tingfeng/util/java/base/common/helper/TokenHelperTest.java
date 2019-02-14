e579323188efd08c39b6068d804fd1a2d92d616b
huitoukest <huitoukest@foxmail.com>
2019-02-14 10:08:22 +0800

    TokenHelper增加 \\ 与\\,内容的转义支持

--- src/test/java/com/tingfeng/util/java/base/common/helper/TokenHelperTest.java
+++ src/test/java/com/tingfeng/util/java/base/common/helper/TokenHelperTest.java
@@ -1,6 +1,8 @@
 package com.tingfeng.util.java.base.common.helper;
 
+import com.alibaba.fastjson.JSON;
 import com.tingfeng.util.java.base.common.utils.CollectionUtils;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -8,37 +10,66 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class TokenHelperTest {
-    static ArrayList<String> list = new ArrayList<>();
-    static{
+
+    static TokenHelper tokenHelper = new TokenHelper();
+
+    @Test
+    public void test(){
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("\\");
+        list.add("\\");
+        list.add("123456");
+        list.add("\\");
+        test(true,list);
+        System.out.println();
+        list.clear();
+
+        list.add("\\,");
+        list.add("\\");
+        list.add("123456");
+        list.add("\\,");
+        test(true,list);
+        System.out.println();
+        list.clear();
+
+        list.add("");
+        list.add("\\");
+        list.add("123456");
+        list.add("\\,");
+        list.add("");
+        list.add("");
+        list.add("");
+        test(true,list);
+        System.out.println();
+        list.clear();
         //分别存入空串，用户id，过期时间，token类型，空串
         list.add("");
         list.add("");
+        list.add("123456");
+        list.add("\\,");
         list.add("123456");
         list.add(String.valueOf(System.currentTimeMillis()));
         list.add("2");
         list.add(",,5,6,7,,");
         list.add("");
+        list.add("\\");
         list.add("");
-    }
-    TokenHelper tokenHelper = new TokenHelper();
 
-    public static void main(String[] args){
-        new TokenHelperTest().testSpeed();
+        test(true,list);
     }
 
-    @Test
-    public void test(){
-        test(true);
-    }
+    public void test(boolean isPrint,ArrayList<String> list) {
 
-    public void test(boolean isPrint) {
 
         String token  = tokenHelper.getToken(list,"123456");
 
         List<String> obj = tokenHelper.parseToken(token,(contents)->contents.get(2),(contents)->contents);
         if(isPrint) {
-            System.out.println(CollectionUtils.join(obj,"$"));
+            System.out.println(JSON.toJSONString(obj));
         }
+        Assert.assertEquals(JSON.toJSONString(obj),JSON.toJSONString(list));
     }
 
     @Test
@@ -46,9 +77,9 @@ public class TokenHelperTest {
         long startTime = System.currentTimeMillis();
         for(int i = 0;i < 10000000;i++){
             if(i % 10000 == 0) {
-                test(true);
+                test();
             }else {
-                test(false);
+                test();
             }
         }
         System.out.println("use time:" + (System.currentTimeMillis() - startTime));
@@ -62,9 +93,9 @@ public class TokenHelperTest {
          new Thread(()->{
              for (int j = 0; j < 500000; j++) {
                  if (value.get() % 100000 == 0) {
-                     test(true);
+                     test();
                  } else {
-                     test(false);
+                     test();
                  }
                  value.incrementAndGet();
              }
