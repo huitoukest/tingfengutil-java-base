05dcdb29ab87fbd90eb30769e87eced0a91bc7e1
huitoukest <huitoukest@foxmail.com>
2019-04-22 09:53:14 +0800

    add

--- src/main/java/com/tingfeng/util/java/base/common/utils/ThrowableUtils.java
+++ src/main/java/com/tingfeng/util/java/base/common/utils/ThrowableUtils.java
@@ -4,7 +4,12 @@ package com.tingfeng.util.java.base.common.utils;
  * 异常处理的一个工具类
  */
 public class ThrowableUtils {
-
+    /**
+     * 获取异常信息
+     * @param e
+     * @param defaultMsg
+     * @return
+     */
     public static String getErrorMsg(Throwable e,String defaultMsg) {
         try {
             if(null != e){
@@ -18,5 +23,26 @@ public class ThrowableUtils {
             return defaultMsg;
         }
         return defaultMsg;
+    }
+
+    /**
+     * 获取特定的Throwable
+     * @param throwable
+     * @param exception
+     * @param <T>
+     * @return
+     */
+    public <T extends Throwable> T getThrowable(Throwable throwable,Class<T> exception){
+        Throwable tmp = throwable;
+        while (tmp != null){
+            if(tmp.getClass().getName().equals(exception.getName())){
+                return (T)tmp;
+            }
+            if(tmp.equals(tmp.getCause())){
+                return null;
+            }
+            tmp = tmp.getCause();
+        }
+        return null;
     }
 }
