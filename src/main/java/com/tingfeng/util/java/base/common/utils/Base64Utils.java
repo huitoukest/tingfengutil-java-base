db06f4c6e8e1e3ee57a3fcb179591428b215f45e
huitoukest <huitoukest@foxmail.com>
2019-06-03 15:49:47 +0800

    add

--- src/main/java/com/tingfeng/util/java/base/common/utils/Base64Utils.java
+++ src/main/java/com/tingfeng/util/java/base/common/utils/Base64Utils.java
@@ -3,6 +3,7 @@ package com.tingfeng.util.java.base.common.utils;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.util.Base64;
+import java.util.stream.IntStream;
 
 /**
  * base64的工具类，使用jdk自带的base64工具
@@ -41,4 +42,53 @@ public class Base64Utils {
         return new String(deCode(content),Charset.forName("utf-8"));
     }
 
+    /**
+     * base64 字符串加密为geturl安全的字符串
+     * @param base64String
+     * @return
+     */
+    public static String enCodeBase64ToSafeUrl(String base64String){
+        StringBuilder sb = new StringBuilder((int) (base64String.length() * 1.5));
+        char[] chars = base64String.toCharArray();
+        IntStream.range(0,chars.length).forEach(it ->{
+            char c = chars[it];
+            switch (c){
+                case '+' : sb.append('-');
+                    break;
+                case '/' : sb.append('_');
+                    break;
+                case '=' :
+                    break;
+                default:
+                    sb.append(c);
+            }
+        });
+        return sb.toString();
+    }
+
+    /**
+     * 解密安全的base64 url 字符串为普通的base64字符串
+     * @param safeBase64UrlStr
+     * @return
+     */
+    public static String decodeSafUrlToBase64(String safeBase64UrlStr){
+        StringBuilder sb = new StringBuilder((int) (safeBase64UrlStr.length() * 1.5));
+        char[] chars = safeBase64UrlStr.toCharArray();
+        IntStream.range(0,chars.length).forEach(it ->{
+            char c = chars[it];
+            switch (c){
+                case '-' : sb.append('+');
+                    break;
+                case '_' : sb.append('/');
+                    break;
+                default:
+                    sb.append(c);
+            }
+        });
+        int mod4 = safeBase64UrlStr.length() % 4;
+        if( mod4 > 0){
+            sb.append("====".substring(mod4));
+        }
+        return sb.toString();
+    }
 }
