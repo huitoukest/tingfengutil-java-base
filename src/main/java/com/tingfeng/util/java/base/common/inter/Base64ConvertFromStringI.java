6358bc1b21341524b1beeb9c02fea8c2234355ed
huitoukest <huitoukest@foxmail.com>
2020-06-09 14:29:21 +0800

    新增Map reverse 方法
    Signed-off-by: huitoukest <huitoukest@foxmail.com>

--- src/main/java/com/tingfeng/util/java/base/common/inter/Base64ConvertFromStringI.java
+++ src/main/java/com/tingfeng/util/java/base/common/inter/Base64ConvertFromStringI.java
@@ -3,6 +3,7 @@ package com.tingfeng.util.java.base.common.inter;
 /**
  * String 转为 base64
  */
+@FunctionalInterface
 public interface Base64ConvertFromStringI {
-	public byte[] convertToBase64(String s);
+	byte[] convertToBase64(String s);
 }
--- src/main/java/com/tingfeng/util/java/base/common/utils/MapUtils.java
+++ src/main/java/com/tingfeng/util/java/base/common/utils/MapUtils.java
@@ -1,8 +1,11 @@
 package com.tingfeng.util.java.base.common.utils;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import com.tingfeng.util.java.base.common.bean.tuple.Tuple2;
+
+import java.util.*;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * 封装和Map相关的处理工具
@@ -80,5 +83,52 @@ public class MapUtils {
         return newHashMap(Arrays.asList(key),value);
     }
 
+    /**
+     * Map 的KV反转
+     * @param originMap
+     * @param mergeFunction 值相同时的处理策略
+     * @param <K>
+     * @param <V>
+     * @return
+     */
+    public static <K,V> Map<V,K> reverse(Map<K,V> originMap, BinaryOperator<K> mergeFunction){
+        return originMap.entrySet()
+                .stream()
+                .collect(Collectors.toMap(entry -> entry.getValue(), entry -> entry.getKey(), mergeFunction));
+    }
 
+    /**
+     * Map的KV反转, 其中V是一个Collection类型.
+     * @param originMap
+     * @param <K>
+     * @param <V>
+     * @return
+     */
+    public static <K,V> Map<V, List<K>> reverses(Map<K,Collection<V>> originMap){
+        return originMap.entrySet()
+                .stream()
+                .filter(it -> null != it.getValue())
+                .flatMap(it -> it.getValue().stream().map(vs -> new Tuple2(it.getKey(),vs)))
+                .collect(Collectors.groupingBy(it -> (V)it.get_2(),Collectors.mapping(it -> (K)it.get_1(),Collectors.toList())));
+    }
+
+    /**
+     * 计算Map KV值 , 支持类型变化; 默认使用Object的KV类型计算,最后强转类
+     * @param originMap
+     * @param kMapperF if null , not use , keep origin type ;
+     * @param vMapperF if null , not use , keep origin type ;
+     * @param <K_R> 返回的K类型
+     * @param <V_R> 返回的V类型
+     * @param <K_O> 输入的原始的K类型
+     * @param <V_O> 输入的原始的V类型
+     * @return
+     */
+    public static <K_R,V_R,K_O,V_O> HashMap<K_R,V_R> compute(Map<K_O,V_O> originMap, Function<K_O,K_R> kMapperF,Function<V_O,V_R> vMapperF){
+        HashMap<Object, Object> map = new HashMap(originMap.size());
+        originMap.forEach((k,v) -> {
+            map.put(Optional.ofNullable(kMapperF).map(it -> (Object) it.apply(k)).orElse(k)
+                    ,Optional.ofNullable(vMapperF).map(it -> (Object)it.apply(v)).orElse(v));
+        });
+        return (HashMap<K_R, V_R>) map;
+    }
 }
