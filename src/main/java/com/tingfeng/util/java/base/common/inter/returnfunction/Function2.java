4760cee56a83bb864bcb958b6b095469f5f16d7c
huitoukest <huitoukest@foxmail.com>
2020-12-07 14:01:28 +0800

    function提供便捷名称
    Signed-off-by: huitoukest <huitoukest@foxmail.com>

---
+++ src/main/java/com/tingfeng/util/java/base/common/inter/returnfunction/Function2.java
@@ -0,0 +1,14 @@
+package com.tingfeng.util.java.base.common.inter.returnfunction;
+
+/**
+ * 以v结尾表示void，没有返回值，以r结尾表示result，有返回值
+ * @author huitoukest
+ *
+ * @param <R> 返回类型
+ * @param <P1> px表示第x个参数,即params x
+ * @param <P2> px表示第x个参数,即params x
+ */
+@FunctionalInterface
+public interface Function2<R,P1,P2> extends FunctionRTwo<R,P1,P2>{
+
+}
