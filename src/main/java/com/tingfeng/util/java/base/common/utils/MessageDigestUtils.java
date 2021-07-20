7486186e10588e2766058c41e08ebfc412d76084
huitoukest <huitoukest@foxmail.com>
2021-07-20 14:10:48 +0800

    MessageDigestUtils 加入流式处理方法
    Signed-off-by: huitoukest <huitoukest@foxmail.com>

--- src/main/java/com/tingfeng/util/java/base/common/utils/MessageDigestUtils.java
+++ src/main/java/com/tingfeng/util/java/base/common/utils/MessageDigestUtils.java
@@ -3,11 +3,14 @@ package com.tingfeng.util.java.base.common.utils;
 
 import com.tingfeng.util.java.base.common.helper.FixedPoolHelper;
 import com.tingfeng.util.java.base.common.helper.SimplePoolHelper;
+import com.tingfeng.util.java.base.common.inter.returnfunction.FunctionROne;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.Mac;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.security.InvalidKeyException;
 import java.security.MessageDigest;
@@ -16,17 +19,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MessageDigestUtils {
+    private static final int DEFAULT_BUFFER_SIZE = 4096;
     private static final int DEFAULT_MAX_MESSAGE_DIGEST_SIZE = 16;
     private static final char[] DIGITS = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
     /**
      * 用来缓存MessageDigest的资源
      */
     private static Map<String, FixedPoolHelper<MessageDigest>> messageDigestMap = new HashMap<>();
+    @Deprecated
     public enum  SHAType{
         SHA512("SHA-512"),SHAMAC512("HmacSHA512"),SHA256("SHA-256"),SHAMAC256("HmacSHA256"),
         SHA1("SHA-1"),MD5("MD5");
         String value;
         SHAType(String value){
+            this.value = value;
+        }
+        public String getValue(){
+            return this.value;
+        }
+    }
+    public enum  DigestType{
+        SHA512("SHA-512"),SHAMAC512("HmacSHA512"),SHA256("SHA-256"),SHAMAC256("HmacSHA256"),
+        SHA1("SHA-1"),MD5("MD5");
+        String value;
+        DigestType(String value){
             this.value = value;
         }
         public String getValue(){
@@ -168,8 +184,29 @@ public class MessageDigestUtils {
      * @param plainText
      * @return
      */
+    @Deprecated
     public static byte[] MD5(byte[] plainText) {
-        return digest("md5",plainText);
+        return digest(SHAType.MD5.getValue(),plainText);
+    }
+    /**
+     * 1.消息摘要算法，MD家族，有MD2 MD4 MD5，其中MD4 JDK不支持
+     *
+     * @param plainText
+     * @return
+     */
+    public static byte[] md5(byte[] plainText) {
+        return digest(SHAType.MD5.getValue(),plainText);
+    }
+    /**
+     * 2.SHA Security Hash Algorithm 安全散列算法，固定长度摘要信息 SHA-1 SHA-2( SHA-224
+     * SHA-256 SHA-384 SHA-512) 使用的依然是MessageDigest类，JDK不支持224
+     *
+     * @param plainText
+     * @return
+     */
+    @Deprecated
+    public static byte[] SHA(SHAType shaType,byte[] plainText) {
+        return digest(shaType.getValue(),plainText);
     }
 
     /**
@@ -179,10 +216,16 @@ public class MessageDigestUtils {
      * @param plainText
      * @return
      */
-    public static byte[] SHA(SHAType shaType,byte[] plainText) {
+    public static byte[] sha(SHAType shaType,byte[] plainText) {
         return digest(shaType.getValue(),plainText);
     }
 
+    /**
+     * apply to small file
+     * @param algorithmName
+     * @param plainText
+     * @return
+     */
     private static byte[] digest(String algorithmName,byte[] plainText){
         FixedPoolHelper<MessageDigest> simplePoolHelper = getMessageDigest(algorithmName);
         return simplePoolHelper.run(digest->{
@@ -191,11 +234,37 @@ public class MessageDigestUtils {
         });
     }
 
+    /**
+     * apply to small file
+     * @param shaType
+     * @param plainText
+     * @param salt
+     * @return
+     */
+    @Deprecated
     public static byte[] SHA(SHAType shaType,byte[] plainText,String salt) {
         return hash(shaType.getValue(),plainText,salt.getBytes(),1);
     }
 
-    public static byte[] SHA(SHAType shaType,String plainText,String salt) {
+    /**
+     * apply to small file
+     * @param shaType
+     * @param plainText
+     * @param salt
+     * @return
+     */
+    public static byte[] sha(SHAType shaType,byte[] plainText,String salt) {
+        return hash(shaType.getValue(),plainText,salt.getBytes(),1);
+    }
+
+    /**
+     * apply to small file
+     * @param shaType
+     * @param plainText
+     * @param salt
+     * @return
+     */
+    public static byte[] sha(SHAType shaType,String plainText,String salt) {
         return hash(shaType.getValue(),plainText.getBytes(),salt.getBytes(),1);
     }
 
@@ -209,6 +278,7 @@ public class MessageDigestUtils {
      * @param plainText
      * @return
      */
+    @Deprecated
     public static byte[] MACSHA(SHAType shaType,byte[] plainText) {
 
         try {
@@ -222,8 +292,56 @@ public class MessageDigestUtils {
         }
     }
 
+    /**
+     * 3.MAC(Message Authentication Code) 消息认证码算法，是含有密钥散列函数算法。
+     * 兼容了MD和SHA的特性。
+     * 加密过程三步走，与后面要介绍的对称加密和非对称加密是相似的
+     * 1) 传入算法，实例化一个加密器
+     * 2) 传入密钥，初始化加密器
+     * 3) 调用doFinal方法进行加密
+     * @param plainText
+     * @return
+     */
+    public static byte[] macSha(SHAType shaType,byte[] plainText) {
 
+        try {
+            byte[] secretBytes = generatorMACSecretKey(shaType);
+            SecretKey key = restoreMACSecretKey(shaType,secretBytes);
+            Mac mac = Mac.getInstance(shaType.getValue());
+            mac.init(key);
+            return mac.doFinal(plainText);
+        } catch (NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * apply to small file
+     * @param shaType
+     * @param plainText
+     * @param secretBytes
+     * @return
+     */
+    @Deprecated
     public static byte[] MACSHA(SHAType shaType,byte[] plainText,byte[] secretBytes) {
+        try {
+            SecretKey key = restoreMACSecretKey(shaType,secretBytes);
+            Mac mac = Mac.getInstance(shaType.getValue());
+            mac.init(key);
+            return mac.doFinal(plainText);
+        } catch (NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * apply to small file
+     * @param shaType
+     * @param plainText
+     * @param secretBytes
+     * @return
+     */
+    public static byte[] macSha(SHAType shaType,byte[] plainText,byte[] secretBytes) {
         try {
             SecretKey key = restoreMACSecretKey(shaType,secretBytes);
             Mac mac = Mac.getInstance(shaType.getValue());
@@ -256,8 +374,69 @@ public class MessageDigestUtils {
      * @param secretBytes
      * @return
      */
+    @Deprecated
     private static SecretKey restoreMACSecretKey(SHAType shaType,byte[] secretBytes) {
         SecretKey key = new SecretKeySpec(secretBytes, shaType.getValue());
         return key;
+    }
+
+    /**
+     * 还原密钥
+     *
+     * @param secretBytes
+     * @return
+     */
+    private static SecretKey restoreMacSecretKey(SHAType shaType,byte[] secretBytes) {
+        SecretKey key = new SecretKeySpec(secretBytes, shaType.getValue());
+        return key;
+    }
+
+    /**
+     * 流式处理
+     * apply to big file, when end won't close the stream
+     * @param algorithmName
+     * @param contentFiller ,to use  digest.update method fill content
+     * @return
+     */
+    public static byte[] digest(String algorithmName, FunctionROne<byte[],MessageDigest> contentFiller){
+        FixedPoolHelper<MessageDigest> simplePoolHelper = getMessageDigest(algorithmName);
+        return simplePoolHelper.run(digest->{
+            digest.reset();
+            contentFiller.run(digest);
+            return digest.digest();
+        });
+    }
+
+    /**
+     * 流式处理
+     * apply to big file, when end won't close the stream
+     * @param algorithmName
+     * @param inputStream
+     * @return
+     */
+    public static byte[] digest(String algorithmName, InputStream inputStream){
+        FixedPoolHelper<MessageDigest> simplePoolHelper = getMessageDigest(algorithmName);
+        return simplePoolHelper.run(digest->{
+            digest.reset();
+            byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+            int length;
+            try {
+                while ((length = inputStream.read(buffer)) != -1) {
+                    digest.update(buffer, 0, length);
+                }
+            }catch (IOException e){
+                throw new com.tingfeng.util.java.base.common.exception.io.IOException (e);
+            }
+            return digest.digest();
+        });
+    }
+
+    /**
+     * apply to big file, when end won't close the stream
+     * @param inputStream
+     * @return
+     */
+    public static byte[] md5(InputStream inputStream){
+        return digest(SHAType.MD5.getValue(), inputStream);
     }
 }
