f126690cb0c426a1d98e81fc2547bbd392ef2314
huitoukest <huitoukest@foxmail.com>
2023-12-15 11:33:56 +0800

    bugfix-EnumUtils同一个Enum多值时bug

--- src/test/java/com/tingfeng/util/java/base/common/utils/EnumUtilsTest.java
+++ src/test/java/com/tingfeng/util/java/base/common/utils/EnumUtilsTest.java
@@ -7,25 +7,34 @@ import org.junit.Test;
 public class EnumUtilsTest {
 
     enum Name implements IEnum<String>{
-        A("A"),
-        B("B"),
-        C("C");
+        A("A","123"),
+        B("B","456"),
+        C("C","789");
 
         private String value;
+        private String code;
 
-        Name(String value){
+        Name(String value,String code){
             this.value = value;
+            this.code = code;
         }
 
         @Override
         public String getValue() {
             return this.value;
         }
+
+        public String getCode(){
+            return this.code;
+        }
     }
 
     @Test
     public void getEnumByValueTest(){
+        Assert.assertTrue(Name.B.equals(EnumUtils.getEnum(Name.class,"456",Name::getCode)));
         Assert.assertTrue(Name.A.equals(EnumUtils.getEnumByValue(Name.class,"A")));
+        Assert.assertTrue(Name.A.equals(EnumUtils.getEnum(Name.class,"123",Name::getCode)));
+        Assert.assertTrue(Name.C.equals(EnumUtils.getEnumByValue(Name.class,"C")));
     }
 
 }
