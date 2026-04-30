1ce7af831f01fa890b0275b74f877f558c04ed04
huitoukest <huitoukest@foxmail.com>
2026-04-30 18:00:22 +0800

    add UrlConverters

--- src/main/java/com/tingfeng/util/java/base/bean/converter/defaults/UrlConverters.java
+++ src/main/java/com/tingfeng/util/java/base/bean/converter/defaults/UrlConverters.java
@@ -2,9 +2,12 @@ package com.tingfeng.util.java.base.bean.converter.defaults;
 
 import com.tingfeng.util.java.base.bean.converter.ConverterRegistry;
 import com.tingfeng.util.java.base.bean.converter.ConverterUtils;
+import lombok.SneakyThrows;
 
-import static com.tingfeng.util.java.base.lang.StringUtils.encodeURL;
-import static com.tingfeng.util.java.base.lang.StringUtils.toDecodeStringUrl;
+import java.io.File;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Paths;
 
 /**
  * URL 编码转换器注册
@@ -13,16 +16,22 @@ public final class UrlConverters {
 
     private UrlConverters() {}
 
+    @SneakyThrows
     public static void register(ConverterRegistry registry) {
         registry.register(ConverterUtils.of(
-                String.class, String.class, 30,
-                s -> s != null,
-                s -> encodeURL(s, "UTF-8")
+                URL.class, String.class, URL::toString
         ));
         registry.register(ConverterUtils.of(
-                String.class, String.class, 30,
-                s -> s != null,
-                s -> toDecodeStringUrl(s, "UTF-8")
+                URI.class, String.class, URI::toString
+        ));
+        registry.register(ConverterUtils.of(
+                URI.class, URL.class, URI::toURL
+        ));
+        registry.register(ConverterUtils.of(
+                URL.class, URI.class, URL::toURI
+        ));
+        registry.register(ConverterUtils.of(
+                URI.class, File.class, uri -> Paths.get(uri).toFile()
         ));
     }
 }
